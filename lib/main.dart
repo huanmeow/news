c354c61eb5f5d038faa868129e3361d247c515ef
MeoHuan Music App <maihuanbumchiu@gmail.com>
2025-03-08 10:14:52 +0700

    news

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'news_provider.dart';
-import 'news_screen.dart';
-import 'theme_provider.dart';
+
+import 'Screen/home_screen.dart';
+
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => NewsProvider()),
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-      ],
-      child: MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: context.watch<ThemeProvider>().themeMode, // Áp dụng ThemeProvider nếu có
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      home: NewsScreen(), // Đặt màn hình chính
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NewsHomeScreen(),
     );
   }
 }
